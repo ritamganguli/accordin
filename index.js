c57cd0277e82409d63a6144a302932252d0c0e44
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2023-02-26 20:50:48 +0530

    accordin

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const ourItems=document.getElementsByClassName("item");
 const openIcons=document.getElementsByClassName("icon_plus");
 const closeIcons=document.getElementsByClassName("icon_minus");
-for (let i=0;i<ourItems.length;i++){
+for (let i=0;i<=ourItems.length;i++){
     closeIcons[i].style.display="none";
     ourItems[i].addEventListener("click",()=>{
-        ourItems[i].classList.toggle("active");
+        const result=ourItems[i].classList.toggle("active");
+        if(result){
+            closeIcons[i].style.display="block";
+            openIcons[i].style.display="none";
+        }else{
+            closeIcons[i].style.display="none";
+            openIcons[i].style.display="block";
+        }
     })
 }
